20182d8ba699f91294d74858e0ce58770aa9f681
shashank-kulkarni9 <20951a05h5@iare.ac.in>
2023-05-30 18:41:25 +0530

    Deployement Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import './style.css';
-import * as CryptoJS from 'crypto-js';
-import { saveAs } from 'file-saver';
+import React, { useState } from "react";
+import "./style.css";
+import * as CryptoJS from "crypto-js";
+import { saveAs } from "file-saver";
 
 function App() {
-  const [key, setKey] = useState('');
+  const [key, setKey] = useState("");
   const [file, setFile] = useState(null);
-  const [result, setResult] = useState('');
-  const [error, setError] = useState('');
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
 
   const handleKeyChange = (e) => {
     setKey(e.target.value);
@@ -22,14 +22,17 @@ function App() {
     if (key && file) {
       const reader = new FileReader();
       reader.onload = function (e) {
-        const fileData = e.target.result.split(',')[1];
+        const fileData = e.target.result.split(",")[1];
         const encryptedData = encrypt(key, fileData);
-        const encryptedFile = createFileFromData(encryptedData, file.name + '.enc');
+        const encryptedFile = createFileFromData(
+          encryptedData,
+          file.name + ".enc"
+        );
         displayResult(encryptedFile);
       };
       reader.readAsDataURL(file);
     } else {
-      displayError('Error: Please provide a key and select a file.');
+      displayError("Error: Please provide a key and select a file.");
     }
   };
 
@@ -37,30 +40,37 @@ function App() {
     if (key && file) {
       const reader = new FileReader();
       reader.onload = function (e) {
-        const fileData = e.target.result.split(',')[1];
+        const fileData = e.target.result.split(",")[1];
         try {
           const decryptedData = decrypt(key, fileData);
-          const decryptedFile = createFileFromData(decryptedData, file.name.replace('.enc', ''));
+          const decryptedFile = createFileFromData(
+            decryptedData,
+            file.name.replace(".enc", "")
+          );
           displayResult(decryptedFile);
         } catch (error) {
-          displayError('Error: Invalid ciphertext');
+          displayError("Error: Invalid ciphertext");
         }
       };
       reader.readAsDataURL(file);
     } else {
-      displayError('Error: Please provide a key and select a file.');
+      displayError("Error: Please provide a key and select a file.");
     }
   };
 
   const encrypt = (key, data) => {
     const keyBytes = CryptoJS.enc.Utf8.parse(key);
-    const encrypted = CryptoJS.AES.encrypt(data, keyBytes, { mode: CryptoJS.mode.ECB }).toString();
+    const encrypted = CryptoJS.AES.encrypt(data, keyBytes, {
+      mode: CryptoJS.mode.ECB,
+    }).toString();
     return encrypted;
   };
 
   const decrypt = (key, ciphertext) => {
     const keyBytes = CryptoJS.enc.Utf8.parse(key);
-    const decrypted = CryptoJS.AES.decrypt(ciphertext, keyBytes, { mode: CryptoJS.mode.ECB });
+    const decrypted = CryptoJS.AES.decrypt(ciphertext, keyBytes, {
+      mode: CryptoJS.mode.ECB,
+    });
     const decryptedData = CryptoJS.enc.Utf8.stringify(decrypted); // Convert decrypted data to string
     return decryptedData;
   };
@@ -79,32 +89,52 @@ function App() {
 
   const displayResult = (file) => {
     setResult(`Result: ${file.name}`);
-    setError('');
+    setError("");
 
     // Trigger file download
     saveAs(file);
   };
 
   const displayError = (errorMsg) => {
-    setResult('');
+    setResult("");
     setError(errorMsg);
   };
 
   return (
-    <div className="container">
-      <h1>AES Encryption/Decryption</h1>
-      <form id="aesForm" encType="multipart/form-data">
-        <label htmlFor="key">Key:</label>
-        <input type="text" name="key" id="key" value={key} onChange={handleKeyChange} required />
-        <label htmlFor="file">Select an image file:</label>
-        <input type="file" name="file" id="file" accept="image/*" onChange={handleFileChange} required />
-        <div className="action-buttons">
-          <input type="submit" value="Encrypt" onClick={handleEncrypt} />
-          <input type="button" value="Decrypt" onClick={handleDecrypt} />
+    <div className="container center">
+      <div>
+        <h1>AES Encryption/Decryption</h1>
+        <form id="aesForm" encType="multipart/form-data">
+          <label htmlFor="key">Key:</label>
+          <input
+            type="text"
+            name="key"
+            id="key"
+            value={key}
+            onChange={handleKeyChange}
+            required
+          />
+          <label htmlFor="file">Select an image file:</label>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            accept="image/*"
+            onChange={handleFileChange}
+            required
+          />
+          <div className="action-buttons">
+            <input type="submit" value="Encrypt" onClick={handleEncrypt} />
+            <input type="button" value="Decrypt" onClick={handleDecrypt} />
+          </div>
+        </form>
+        <div id="resultDiv" className="result">
+          {result}
         </div>
-      </form>
-      <div id="resultDiv" className="result">{result}</div>
-      <div id="errorDiv" className="error">{error}</div>
+        <div id="errorDiv" className="error">
+          {error}
+        </div>
+      </div>
     </div>
   );
 }
